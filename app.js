cf9de7d09b9bb5c12df8d4ad502f2127a564902b
Sushmitha Rajendran <USH2COB@rbeigcn.com>
2019-05-09 11:09:16 +0530

    simple database

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+var port = 4000;
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb+srv://admin:pFeFV7zC7S08mhAW@mongodb-app-svdvh.mongodb.net/MyCinemaDB?retryWrites=true",
+  { useNewUrlParser: true }
+);
+
+var theatreSchema = new mongoose.Schema({
+  theatreName: String,
+  screenCount: Number
+});
+
+var AddTheatre = mongoose.model("theatres", theatreSchema);
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addTheatre", (request, response) => {
+  var myData = new AddTheatre(request.body);
+  myData
+    .save()
+    .then(item => {
+      response.send(item);
+    })
+    .catch(error => {
+      response.status(400).send("Not able to save!");
+    });
+});
+
+app.listen(port, () => {
+  console.log("Cinema is listenng for actions on port: ", port);
+});
